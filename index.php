192e8870d039c86d50f6d50b0408750e1ee55c05
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-10 16:11:25 +0200

    add table in page

--- index.php
+++ index.php
@@ -70,26 +70,65 @@ $hotels = [
 
 <body>
 
-    <ul>
-        <?php foreach ($hotels as $key => $value) : ?>
-            <li>
-                <?php echo $value['name']; ?>
-                <ul>
-                    <li>
-                        <?php echo $value['description']; ?>
-                    </li>
-                    <li>
-                        <?php echo 'voto ' . $value['vote'] . ' su 5' ?>
-                    </li>
-                    <li>
-                        <?php echo $value['distance_to_center'] . ' km'; ?>
-                    </li>
-                </ul>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="d-flex align-items-center pt-2">
+        <ul>
+            <?php foreach ($hotels as $key => $value) : ?>
+                <li>
+                    <?php echo $value['name']; ?>
+                    <ul>
+                        <li>
+                            <?php echo $value['description']; ?>
+                        </li>
+                        <li>
+                            <?php echo 'voto ' . $value['vote'] . ' su 5' ?>
+                        </li>
+                        <li>
+                            <?php echo $value['distance_to_center'] . ' km'; ?>
+                        </li>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <div class="container">
 
 
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($hotels as $key => $value) : ?>
+                        <tr>
+                            <td>
+                                <?= $value['name']; ?>
+                            </td>
+
+                            <td>
+                                <?= $value['description']; ?>
+                            </td>
+
+                            <td>
+                                <?= $value['vote']; ?>
+                            </td>
+
+                            <td>
+                                <?= $value['distance_to_center'] . ' km'; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+        </div>
+
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
